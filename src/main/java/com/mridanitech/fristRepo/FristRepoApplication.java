6d082473c0dd15bfd8e883933c9787d1256e9ee2
mridanitechno <47944427+mridanitechno@users.noreply.github.com>
2020-02-26 18:45:53 +0530

    spring boot web and actuator

---
+++ src/main/java/com/mridanitech/fristRepo/FristRepoApplication.java
@@ -0,0 +1,13 @@
+package com.mridanitech.fristRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mridanitech/fristRepo/FristRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mridanitech.fristRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
